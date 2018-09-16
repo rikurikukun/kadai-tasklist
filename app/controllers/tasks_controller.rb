61e0bc15ac347f8e92f516819ac65989caed08ee
rikuto yoshida <git.rubyandmarkup@gmail.com>
2018-09-16 16:16:03 +0000

    kadaidesu917116

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
       redirect_to @task
     else
       flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new 
+      render :edit
     end
 
   def edit
