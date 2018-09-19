17c7ecbaf240a59d94f4f47c3abaea20afde97fb
rikuto yoshida <git.rubyandmarkup@gmail.com>
2018-09-19 13:22:58 +0000

    8login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
 
   root to: "toppages#index"
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
+  
+  
   
   get 'sessions/new'
   get 'sessions/create'
